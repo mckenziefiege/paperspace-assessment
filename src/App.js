5611d787fc1a57a7600e6a518bd655a13a8d08bd
McKenzie Fiege <mckenziefiege@McKenzies-MBP.home>
2019-08-23 10:19:30 -0400

    Able to fetch markdown gist through API

--- src/App.js
+++ src/App.js
@@ -9,10 +9,17 @@ class App extends Component {
 
   state = {
     modalShown: false,
-    alerts: []
+    alerts: [],
+    files: {}
   }
 
   componentDidMount() {
+    fetch("https://api.github.com/gists/ab0d7ff570882e888acff25943d1a76e")
+      .then(resp => resp.json())
+      .then(resp => this.setState({
+        files: resp.files["releaseNotes.md"]
+      }))
+
     this.setState({
       alerts: [{"title": "Totally new dashboard", "date": "8/20/2019", "type": "New", "content": "We added this amazing new feature. It lets you do amazing new things and so much more. Try it out today to see for yourself how amazing it is!"}, {"title": "Faster load times", "date": "8/14/2019", "type": "Improvement", "content": "We added this amazing new feature. It lets you do amazing new things and so much more. Try it out today to see for yourself how amazing it is!"}]
     })
@@ -25,6 +32,7 @@ class App extends Component {
   }
 
   render() {
+    console.log(this.state.files.content)
     return (
       <div className="App">
         <UserPhoto updateModal={this.updateModal} alerts={this.state.alerts}/>
