ae40288440a7a84a722a3a9ba03da579e0427a11
McKenzie Fiege <mckenziefiege@McKenzies-MBP.home>
2019-08-23 12:53:44 -0400

    Parsed markdown file from github gist API without using npm package. Parsed markdown displayed within modal

--- src/Components/Alert.js
+++ src/Components/Alert.js
@@ -1,16 +1,37 @@
 import React from 'react';
 
-const Alert = (props) => (
-  <div className="alert">
-    <div>
-      <p>{props.alert.date}</p>
+function Alert (props) {
+
+  let newArr = []
+  for (let i = 0; i < props.alert.length; i++) {
+    newArr.push(props.alert[i].split('#'))
+  }
+
+  let arr = []
+  for (let i = 0; i < newArr.length; i++) {
+    arr.push(newArr[i][0])
+  }
+
+  let obj = {}
+
+  obj["title"] = arr[0]
+  obj["type"] = arr[1].trim()
+  obj["date"] = arr[2]
+  obj["content"] = arr[3]
+
+
+  return (
+    <div className="alert">
+      <div>
+        <p>{obj.date}</p>
+      </div>
+      <div>
+        <h1>{obj.title}</h1>
+        <p className={obj.type === "New" ? "alert__type alert__type--blue" : "alert__type alert__type--green"}>{obj.type}</p>
+        <p>{obj.content}</p>
+      </div>
     </div>
-    <div>
-      <h1>{props.alert.title}</h1>
-      <p className={props.alert.type === "New" ? "alert__type alert__type--blue" : "alert__type alert__type--green"}>{props.alert.type}</p>
-      <p>{props.alert.content}</p>
-    </div>
-  </div>
-);
+  )
+}
 
 export default Alert;
--- src/Components/UserPhoto.js
+++ src/Components/UserPhoto.js
@@ -2,12 +2,10 @@ import React from 'react';
 import Photo from '../Images/blank-profile-photo.png';
 
 function UserPhoto(props) {
-  let num = props.alerts.length
-
   return (
     <div className="user" onClick={props.updateModal}>
       <img className="user__photo" src={Photo} alt="User"/>
-      {props.alerts !== 0 && <div className="user__alerts">{num}</div>}
+      {props.alerts !== 0 && <div className="user__alerts">{props.alerts}</div>}
     </div>
   );
 
